4706b4707ef66c2d528ba6057e32f8b1bbc1afe9
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2017-05-03 20:12:47 -0300

    default controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/fon", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
     {
